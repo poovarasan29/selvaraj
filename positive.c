4e6e475c91e33c02be29431eb8c118a0ae8b41a2
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2017-09-05 19:07:58 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -1,1 +1,10 @@
-# selvaraj
+#include<studio.h>
+Void main()
+  {
+  Int a=0;
+    Printf("enter the value ");
+  If (a=0)
+Printf(" zero ");
+  Else
+    Printf(" positive or negativenegative");
+  
