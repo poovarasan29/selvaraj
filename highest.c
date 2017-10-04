d29be7e5b22fdc118757b0e05bd6fd52daebdaf3
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2017-10-04 15:36:51 +0530

    Update highest.c

--- highest.c
+++ highest.c
@@ -2,20 +2,22 @@
 int main()
 {
 int a,b,c;
-printf("enter the value:");
-scanf(%d%d%d,&a,&b,&c);
+printf("enter the num: ");
+scanf("%d%d%d",&a,&b,&c);
 if(a<b)
 {
-if(b<c)
-{
-printf("large num:%d,c");
-}
-else if(b>c)
-{
-printf("large num:%d,b");
-}
+   if(b<c)
+  {
+     printf("largest num: %d",c);
+   }
+   else 
+   if(b>c)
+    {
+      printf("largest num: %d",b);
+     }
+    }
 else
 {
-printf("large num:%d,a");
+printf("largest num: %d",a);
 }
-}
+
