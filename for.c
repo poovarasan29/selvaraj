b838f70f60b012a2d7b572bfb2047ab63428baa3
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2017-10-17 21:27:16 +0530

    Update for.c

--- for.c
+++ for.c
@@ -1,25 +1,19 @@
-Program: For Loop
+
 
 #include < stdio.h>
 
 int main()
 {
-    int i;
+    int ii,a=3,b=4,c;
 
-    /* The loop goes while i < 10, and i increases by one every loop*/
+    
 
     for ( i = 0; i < 10; i++ ) {
-
-        /* Keep in mind that the loop condition checks
-           the conditional statement before it loops again.
-           consequently, when i equals 10 the loop breaks.
-           i is updated before the condition is checked. */
-
-        printf( "%d\n", i );
+       c=a+b;
+        {
+        printf( "%d\n,%d\n", i,c );
     }
-    getchar();
+  getchar();
 }
 
--via Programming Hub for Android, a top rated Programming App on Google Play
 
-https://play.google.com/store/apps/details?id=com.freeit.java
