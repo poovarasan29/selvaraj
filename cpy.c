b88e753839f5a17daf04e1bbb1aae4eb2347a7be
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2017-10-24 23:03:40 +0530

    Add files via upload

---
+++ cpy.c
@@ -0,0 +1,37 @@
+Program: Copy String without using strcpy() function
+
+#include<stdio.h>
+
+void stringCopy(char[],char[]);
+
+int main(){
+
+    char str1[100],str2[100];
+
+    printf("Enter any string: ");
+    scanf("%s",str1);
+
+    stringCopy(str1,str2);
+
+    printf("After copying: %s",str2);
+
+    return 0;
+}
+
+void stringCopy(char str1[],char str2[]){
+
+    int i=0;
+
+    while(str1[i]!='\0'){
+         str2[i] = str1[i];
+         i++;
+    }
+
+    str2[i]='\0';
+
+}
+
+
+-via Programming Hub for Android, a top rated Programming App on Google Play
+
+https://play.google.com/store/apps/details?id=com.freeit.java
