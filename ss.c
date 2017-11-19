ab84971a3abdc69e442036099eaa5d30a433f9ca
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2017-11-19 15:46:18 +0530

    Add files via upload

---
+++ ss.c
@@ -0,0 +1,28 @@
+Program: Calculate Square of 'n' numbers
+
+    #include<stdio.h>
+    #include<conio.h>
+    void main()
+    {
+        int n,r,i,sqr=0;
+
+        printf("\nENTER THE RANGE.:= ");
+        scanf("%d",&r);
+
+        for(i=1;i<=r;i++){
+
+        n=i;
+        sqr=n*n;
+        printf("\nSQUARE OF %d is %d .",n,sqr);
+
+        }
+
+
+        getch();
+
+    }
+
+
+-via Programming Hub for Android, a top rated Programming App on Google Play
+
+https://play.google.com/store/apps/details?id=com.freeit.java
