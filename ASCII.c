05fb7a7ca2b3954b4db25327e20d047514e0f0b1
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2017-11-23 11:51:45 +0530

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,16 @@
+Program: ASCII value of a Character
+
+#include <stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a character: ");
+    scanf("%c",&c);
+    printf("ASCII value of %c = %d",c,c);
+    return 0;
+}
+
+
+-via Programming Hub for Android, a top rated Programming App on Google Play
+
+https://play.google.com/store/apps/details?id=com.freeit.java
