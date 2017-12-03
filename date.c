fed4f9bfaa1e48cb279bd0a193014534e766e759
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2017-12-03 09:15:35 +0530

    Update date.c

--- date.c
+++ date.c
@@ -6,10 +6,9 @@ int main()
     time_t t;
     time(&t);
 
-    printf("Today's date and time : %s", ctime(&t));
+    printf("odd day and time : %s", ctime(&t));
 
     return 0;
 }
 
 
-My lesson highlights! Check it out on your phone in Programming Hub: https://goo.gl/D2iETN
