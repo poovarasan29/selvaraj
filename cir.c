e920953d913da54da8248b590cbf2bff7fd59308
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2017-12-03 22:14:50 +0530

    Add files via upload

---
+++ cir.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int rad;
+    float PI = 3.14, area, ci;
+
+    printf("\nEnter radius of circle: ");
+    scanf("%d", &rad);
+
+    area = PI * rad * rad;
+    printf("\nArea of circle : %f ", area);
+
+    ci = 2 * PI * rad;
+    printf("\nCircumference : %f ", ci);
+
+    return 0;
+}
+
+
+My lesson highlights! Check it out on your phone in Programming Hub: https://goo.gl/D2iETN
