37d996493a7370dbfcd0fb0ed47a3926de4c5de3
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2017-12-04 11:25:04 +0530

    Add files via upload

---
+++ sumdig.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int add_digits(int);
+
+int main()
+{
+    int n, result;
+
+    printf("Enter a number\n");
+    scanf("%d", &n);
+
+    result = add_digits(n);
+
+    printf("%d\n", result);
+
+    return 0;
+}
+
+int add_digits(int n)
+{
+    static int sum = 0;
+
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    sum = n % 10 + add_digits(n / 10);
+
+    return sum;
+}
+
+My lesson highlights! Check it out on your phone in Programming Hub: https://goo.gl/D2iETN
