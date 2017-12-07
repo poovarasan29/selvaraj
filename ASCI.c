933492303aab3ecb22f249af3800c8f5875bce07
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2017-12-07 09:18:06 +0530

    Update and rename ASCII.c to ASCI.c

--- ASCI.c
+++ ASCI.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 int main()
 {
-    char c;
+    char c; int a;
     printf("Enter a character: ");
     scanf("%c",&c);
     printf("ASCII value of %c = %d",c,c);
+    printf("enter the password: ");
+    scanf(" %a",&a);
+    printf(" the password %a =%d",a);
     return 0;
 }
 
