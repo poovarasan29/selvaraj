8381f31b1e9059aff2418f2100bd633ee2acbcc2
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2018-01-18 11:23:50 +0000

    Create space.c

---
+++ space.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+ 
+int main(){
+    char inputString[100], outputArray[100];
+    int readIndex = 0, writeIndex;
+    printf("Enter a String \n");
+    gets(inputString);
+    
+    while(inputString[readIndex] == ' '){
+        readIndex++;
+    }
+ 
+    for(writeIndex = 0;inputString[readIndex] != '\0'; readIndex++){
+      if(inputString[readIndex]==' ' && inputString[readIndex-1]==' '){
+          continue;
+      }
+      outputArray[writeIndex] = inputString[readIndex];
+      writeIndex++;
+    }
+    outputArray[writeIndex] = '\0';
+    printf("String without extra spaces\n%s", outputArray);
+ 
+    getch();
+    return 0;
+}
