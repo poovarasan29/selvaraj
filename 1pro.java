e10d083599ab95bc38926f489e270292eb7a9248
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2018-01-21 13:59:30 +0530

    Create 1pro.java

---
+++ 1pro.java
@@ -0,0 +1,15 @@
+public class Even
+{
+    public static void main(String [] args)
+    {
+     
+    int product = 1;
+         
+        for ( int x = 6; x <= 20; x += 2)
+             
+        product *= x;
+ 
+        System.out.printf("Product is: %d\n", product);
+    }
+     
+}
