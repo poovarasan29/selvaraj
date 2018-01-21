0723e83b2218f93866396d514a8ef30812cfaad3
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2018-01-21 14:52:11 +0530

    Create 1cap.c

---
+++ 1cap.c
@@ -0,0 +1,16 @@
+void main() {
+     char sentence[100];
+     int i;
+
+     printf("Enter your name and surnames: ");
+     gets(sentence);
+
+     for(i = 0; i<strlen(sentence); i++){
+        if(sentence[i] == ' '){
+            printf("%c", toupper(sentence[i]+1)); 
+            
+        } else {
+            printf("%c", sentence[i]);
+        }
+     }
+} 
