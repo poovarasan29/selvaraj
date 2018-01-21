e35312298868cd7430f9dd69c7ccfb1f8ec527c4
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2018-01-21 14:12:34 +0530

    Create 1bitwise.java

---
+++ 1bitwise.java
@@ -0,0 +1,19 @@
+
+public class Swap {
+ 
+public static void main(String args[]) {
+int a = 89, b = 90;
+System.out.println("Before swapping:");
+ 
+System.out.println("A=" + a);
+System.out.println("B=" + b);
+ 
+a = a ^ b; 
+b = a ^ b;
+a = a ^ b; 
+ 
+System.out.println("After swapping:");
+System.out.println("A=" + a);
+System.out.println("B=" + b);
+}
+}
