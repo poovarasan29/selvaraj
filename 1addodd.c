b0db816024d580e7fb4a2c7388866683cebab2ac
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2018-01-25 10:59:21 +0000

    Create 1addodd.c

---
+++ 1addodd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d%d",&a,&b);
+printf("enter the number : ");
+c=a+b;
+printf("%d",&c);
+{
+if(c%2==0)
+printf(" even num");
+else
+printf(" odd num");
+return 0;
+}
+}
