63749f4dd36fb026b0b456f15a17a39a8a84bcb5
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2018-02-01 14:26:11 +0530

    Create 1strin.c

---
+++ 1strin.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[33];
+int i,k;
+printf("enter the k");
+scanf("%d",&k);
+printf("enter the char");
+scanf("%s",&a);
+for(i=0;i<=k;i++)
+{
+printf("%s",&a);
+}
+return 0;
+}
