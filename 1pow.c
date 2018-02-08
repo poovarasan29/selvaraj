a9c141208f0449add6a55f0820bda946cb3aba63
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2018-02-08 14:36:34 +0530

    Create 1pow.c

---
+++ 1pow.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ int n,k,ans;
+printf("enthr the num:");
+scanf("%d",&n);
+printf("enter the power:");
+scanf("%d",&k);
+ans=pow(n,k);
+printf("%d",ans);
+return 0;
+}
+
