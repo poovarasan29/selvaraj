f47b929f52304354ed2754bae73d58f0fb22f689
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2018-02-22 16:19:37 +0530

    Create 2num.c

---
+++ 2num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) 
+{
+char b[100];
+int count=0,i;
+printf("Enter the string:");
+scanf("%[^\n]s",b);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]>='0'&&a[i]<='9')
+count++;
+}
+printf("number numeric character is :%d",count);
+return 0;
+}
