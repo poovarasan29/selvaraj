c2ad28837ae1ee131cb8764e31325a76942e28d7
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2018-02-27 16:54:14 +0530

    Update Char.c

--- Char.c
+++ Char.c
@@ -2,11 +2,15 @@
 int main()
 {
 char ch;
-print("enter the character:");
+printf("enter the character:");
 scanf("%c",&ch);
-if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||char=='U')
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+{
 printf("%c is a vowel.\n",ch);
+}
 else
+{
 printf("%c is not vowel.\n",ch);
+}
 return 0;
 }
