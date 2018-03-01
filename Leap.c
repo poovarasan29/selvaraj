06aec2eb042097db17a2dc78fc7f4298d73e1287
poovarasan29 <31503774+poovarasan29@users.noreply.github.com>
2018-03-01 10:50:39 +0000

    Update Leap.c

--- Leap.c
+++ Leap.c
@@ -1,6 +1,5 @@
-
 #include<stdio.h>
-#include<conio.h>
+
 
 
 int main()
@@ -13,11 +12,14 @@ int main()
    scanf(" %d",&year);
 
    if(year%4==0 || year%400==0 || year%100==0)
+   {
    printf("This is a leap year");
-
+}
    else
+   {
  printf("This is not a leap year");
-
+}
    return 0;
 }
+
 
